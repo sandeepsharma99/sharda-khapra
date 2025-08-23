c2b58cf662cd5aa3a8f1098477bcc0ae22e77600
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-08-23 20:33:10 +0530

    EJS conditiona, loops

--- Backend-1/EJS/index.js
+++ Backend-1/EJS/index.js
@@ -1,16 +1,30 @@
 const express = require("express");
+const path = require("path");
 const app = express()
 
 const port = 3000;
 
-app.set("view engine","ejs")
+app.set("view engine","ejs");
 
+
+app.set("views",path.join(__dirname,"/views"))
 // app.get("/",(req,res)=>{
 //     res.send("home page")    
 // })
 
 app.get("/",(req,res)=>{
     res.render("travelling.ejs")    
+})
+
+app.get("/rolldice",(req,res)=>{
+    let diceVal = Math.floor(Math.random()*6)+1
+    res.render("rolldice.ejs",{diceVal})    
+})
+
+app.get("/ig/:username",(req,res)=>{
+    
+    let {username} = req.params
+    res.render("insta.ejs", {username})
 })
 
 app.listen(port,(req,res)=>{
