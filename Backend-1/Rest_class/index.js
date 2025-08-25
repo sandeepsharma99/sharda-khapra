3e777a502d3a57f87cac117db0c1a6d6bf110f54
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-08-25 12:31:32 +0530

    error in show single quora post

--- Backend-1/Rest_class/index.js
+++ Backend-1/Rest_class/index.js
@@ -13,14 +13,17 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts = [
     {
+    id:"1a",
     username:"sandeep",
     content:"I Love coding"
     },
     {
+    id:"2b",
     username:"ram",
     content:"I Love cricket"
     },
     {
+    id:"3c",
     username:"laxman",
     content:"I Love boxing"
     },
@@ -32,16 +35,24 @@ app.get("/posts",(req,res)=>{
 
 // *creating a form in  new.ejs file and getting*
 
-app.get("/posts/new",(req,res)=>{
-    res.render("new.ejs");
-})
+// app.get("/posts/new",(req,res)=>{
+//     res.render("new.ejs");
+// })
 
-app.post("/posts",(req,res)=>{
-    let { username , content } = req.body;  // req goes within body in post
-    posts.push({username ,content})   // pushing new object to posts array
-    // res.send("post request working")
-    res.redirect("/posts")
-})
+// app.post("/posts",(req,res)=>{
+//     let { username , content } = req.body;  // req goes within body in post
+//     posts.push({username ,content})   // pushing new object to posts array
+//     // res.send("post request working")
+//     res.redirect("/posts")
+// })
+
+app.get("/posts/:id",(req,res)=>{
+    let { id } = req.params;
+    // console.log( id); // See what you are sending
+    let post = posts.find((p) => id === p.id);
+    console.log("found post",post);
+    res.render("show.ejs");
+});
 
 app.listen(port,()=>{
     console.log("listening on port:3000");
