529b1e7c424e804f2cf417fdb0a86d2a0396bbbd
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-08-26 23:08:21 +0530

    method error unable to update

--- Backend-1/Rest_class/index.js
+++ Backend-1/Rest_class/index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({extended :true}));
+app.use(methodOverride("_method"));
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
@@ -58,7 +60,6 @@ app.patch("/posts/:id",(req,res)=>{
     let post = posts.find((p)=> id === p.id);
     post.content = newContent;
     console.log(post);
-    
     // let newContent = rq.body.content;
     console.log(id);
     res.send("patch request working");
