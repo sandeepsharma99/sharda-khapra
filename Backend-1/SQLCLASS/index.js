7715f9ebc4a6b3153d3f471bcbe13ad9d733ba16
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-09-09 21:25:49 +0530

    routing importing express, .get operation

--- Backend-1/SQLCLASS/index.js
+++ Backend-1/SQLCLASS/index.js
@@ -1,5 +1,8 @@
 import { faker } from '@faker-js/faker';
 import mysql from 'mysql2/promise';
+import express from 'express';
+
+const app = express();
 
 // Create the connection to database
 const connection = await mysql.createConnection({
@@ -8,8 +11,6 @@ const connection = await mysql.createConnection({
   database: 'My_app',
   password:'India@12345678'
 });
-/* inserting new data */
-let q = "INSERT INTO user(id,username, email,password) VALUES ?";
 
 let getRandomUser = () => {
   return [
@@ -19,27 +20,46 @@ let getRandomUser = () => {
     faker.internet.password(),
   ];
 }
+/* --- */
 
-let data =[];
-for(let i =1; i<=100; i++){
-  data.push(getRandomUser());  // 100random user data
-}
+app.get("/",(req,res)=>{
+  let q = `select count(*) from user`;
+  try{
+    const [rows] = await connection.query(q);
+    console.log(rows);
+    }catch(err){
+        console.log(err);
+        res.send("some error in database")
+    }
+})
 
+
+
+app.listen("3000", ()=>{
+  console.log("server is listening on 8080")
+})
+
+// to run conection and run query/operations
+
+// try{
+//     const [rows] = await connection.query(q,[data]);
+//     console.log(rows);
+//     }catch(err){
+//         console.log(err);
+//     }
+
+// connection.end();
+
+
+// /*  --- inserting new data */
+// let q = "INSERT INTO user(id,username, email,password) --VALUES ?";
+
+// let data =[];
+// for(let i =1; i<=100; i++){
+//   data.push(getRandomUser());  // 100random user data
+// }
 
 // let users = [
 //              ["01","sand","sandy@gmail.com", "pass123"],
 //              ["02","rma","eg@gmail.com", "pass456"]
 //             ];
-
-// to run conection and run query/operations
-try{
-    const [rows] = await connection.query(q,[data]);
-    console.log(rows);
-    }catch(err){
-        console.log(err);
-    }
-
-connection.end();
-
-
-
